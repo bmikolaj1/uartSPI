3dc48c6a22bc3abfb6f462999333d49626f015a5
DESKTOP-VQSFIM4\Admin <bruno.mikolaj1@gmail.com>
2017-12-07 16:22:16 +0100

    removed tmp qsys files

--- src/unsaved/unsaved_inst.v
+++
@@ -1,6 +0,0 @@
-	unsaved u0 (
-		.clk_clk       (<connected-to-clk_clk>),       //    clk.clk
-		.reset_reset_n (<connected-to-reset_reset_n>), //  reset.reset_n
-		.output_readra (<connected-to-output_readra>)  // output.readra
-	);
-
